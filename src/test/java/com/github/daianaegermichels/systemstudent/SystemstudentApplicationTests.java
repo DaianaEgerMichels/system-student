ff6e0c153fab753a7f76bb4abadc70f567b45331
Daiana Michels <daianaem.12@gmail.com>
2022-06-18 19:43:37 -0300

    first commit

---
+++ src/test/java/com/github/daianaegermichels/systemstudent/SystemstudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.daianaegermichels.systemstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemstudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
